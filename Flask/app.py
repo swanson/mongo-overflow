5f8124051149e346a78205d701397953a1983dcf
Matt Swanson <swanson@purdue.edu>
2010-08-04 23:04:41 -0400

    adding timesince filter for pretty dates

--- Flask/app.py
+++ Flask/app.py
@@ -2,11 +2,40 @@ from flask import Flask, g, session, request, render_template, flash, redirect, 
 from flaskext.openid import OpenID
 from db.documents import User, Question, Answer, QuestionInputForm, AnswerInputForm
 from mongoengine import *
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'very_secret_key'
 oid = OpenID(app)
+
+@app.template_filter()
+def timesince(dt, default="just now"):
+    """
+    Returns string representing "time since" e.g.
+    3 days ago, 5 hours ago etc.
+    """
+
+    now = datetime.now()
+    diff = now - dt
+    
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+
+    for period, singular, plural in periods:
+        
+        if period:
+            return "%d %s ago" % (period, singular if period == 1 else plural)
+
+    return default
 
 @app.before_request
 def lookup_current_user():
